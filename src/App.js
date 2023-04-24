9b0db2957b3863bcd0c1571862daeef3c718d29b
Cassandra Campero <cassandra.campero2@bbva.com>
2023-04-23 19:28:59 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {getTodos} from "./services/index";
 
-function App() {
+function App(){
+  const [serviceData, setServiceData] = useState();
+
+useEffect(()=>{
+  getTodos().then((res)=>setServiceData(res.data));
+  console.log(serviceData);
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> SERVICE RESULT</h1>
+      <hr></hr>
+
+    <ul>
+      {serviceData ? serviceData.map((data)=>(
+        <li key={data.id}>
+          title: {data.title}
+          <br/>
+          userId: {data.userId}
+          <br />
+          completed: {data.completed ? "true": "false"}
+          <hr />
+        </li>
+      )): <h1>Sin información</h1>}
+    </ul>
+
     </div>
   );
 }
